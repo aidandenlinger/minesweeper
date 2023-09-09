5b0d36b343f17527c61e13addb5deb65162be334
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-09-08 20:57:06 -0700

    fix: simplify some of the gridding. navbar is not centering as expected :(

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Navbar from "../Navbar/Navbar.svelte";
   import { create } from "../logic/minesweeper";
-  import Cell from "./Cell.svelte"
+  import Cell from "./Cell.svelte";
 
   const gameState = create(9, 9, 30);
 </script>
@@ -9,24 +9,18 @@
 <Navbar />
 
 <main class="game">
-  <div class="aaa">
-    {#each gameState.game as row, x}
-      <div class="row">
-        {#each row as cell, y}
-          <Cell {cell} {x} {y}/>
-        {/each}
-      </div>
-    {/each}
-  </div>
+  {#each gameState.game as row, x}
+    <div class="row">
+      {#each row as cell, y}
+        <Cell {cell} {x} {y} />
+      {/each}
+    </div>
+  {/each}
 </main>
 
 <style>
-  .game {
+  .row {
     display: flex;
     justify-content: center;
-  }
-  
-  .row {
-    display: flex
   }
 </style>
