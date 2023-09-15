78d29976274ed0b08d8a9643cd52e5af12b6eaf5
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-09-15 12:32:39 -0700

    feat(Cell): neighbors function

--- src/logic/Cell.ts
+++ src/logic/Cell.ts
@@ -1,3 +1,13 @@
 import type { GameState } from "./minesweeper"
 export type Cell = { "status": "hidden" } | { "status": "open" } | { "status": "mine" }
 
+export function* neighbor(gameState: GameState, x: number, y: number): Generator<number[]> {
+  for (let neighX = x - 1; neighX <= x + 1; ++neighX) {
+    if (neighX < 0 || neighX >= gameState.width) continue;
+    for (let neighY = y - 1; neighY <= y + 1; ++neighY) {
+      if (neighY < 0 || neighY >= gameState.height) continue;
+      if (neighX == x && neighY == y) continue;
+      yield [neighX, neighY];
+    }
+  }
+}
