8cbd42341fbf1563e54c3cbaf1e8284df13ce2e1
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-09-15 23:26:39 -0700

    style(frontend): nicer difficulty selection buttons

--- src/frontend/Setup.svelte
+++ src/frontend/Setup.svelte
@@ -7,13 +7,40 @@
 
 <button
   on:click={() => dispatch("setup", { width: 9, height: 9, mineCount: 10 })}
-  in:fade>Beginner</button
+  in:fade
 >
+  <hgroup>
+    <h3>Beginner</h3>
+    <p>(9x9, 10 mines)</p>
+  </hgroup>
+</button>
+
 <button
   on:click={() => dispatch("setup", { width: 16, height: 16, mineCount: 40 })}
-  in:fade>Intermediate</button
+  in:fade
 >
+  <hgroup>
+    <h3>Intermediate</h3>
+    <p>(9x9, 10 mines)</p>
+  </hgroup>
+</button>
+
 <button
   on:click={() => dispatch("setup", { width: 16, height: 30, mineCount: 99 })}
-  in:fade>Advanced</button
+  in:fade
 >
+  <hgroup>
+    <h3>Advanced</h3>
+    <p>(16x30, 99 mines)</p>
+  </hgroup>
+</button>
+
+<style>
+  hgroup {
+    margin: 0;
+  }
+  p {
+    margin: 0;
+    color: white;
+  }
+</style>
