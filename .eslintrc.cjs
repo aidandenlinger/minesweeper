0cf61fc6e153eb771dbd82c03b81a2c3290f6d47
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-10-28 22:21:52 -0700

    fix(eslint): don't use type-checked rules as they lead to false positives with svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict-type-checked', 'plugin:@typescript-eslint/stylistic-type-checked', 'plugin:svelte/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict', 'plugin:@typescript-eslint/stylistic', 'plugin:svelte/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
