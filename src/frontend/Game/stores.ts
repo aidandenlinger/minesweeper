a4787b46e9214e5e35bc1e6e41b59934fc9a151b
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-11-07 19:01:09 -0800

    feat: save settings to local storage

--- src/frontend/Game/stores.ts
+++ src/frontend/Game/stores.ts
@@ -1,8 +1,12 @@
 import { writable, type Writable } from 'svelte/store';
 import type { FirstClickBehavior } from '../../logic/minesweeper';
 
-export const firstClickBehavior: Writable<FirstClickBehavior> = writable("open cell")
+export const firstClickBehavior: Writable<FirstClickBehavior> = writable(localStorage.getItem("firstClickBehavior") as FirstClickBehavior ?? "open cell")
+
+firstClickBehavior.subscribe((val) => localStorage.setItem("firstClickBehavior", val))
 
 export type Theme = "light" | "dark"
 
-export const currentTheme: Writable<Theme> = writable(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+export const currentTheme: Writable<Theme> = writable(localStorage.getItem("currentTheme") as Theme ?? (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"))
+
+currentTheme.subscribe((theme) => localStorage.setItem("currentTheme", theme))
